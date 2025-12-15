0701ebb9eb8aaf386b3973f2bc11f7ea6eec9dfa
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2025-12-15 09:15:14 +0100

    Update STAC version to 1.1.0 and adjust import order in main.py

--- titiler/eopf/openeo/main.py
+++ titiler/eopf/openeo/main.py
@@ -14,13 +14,13 @@ from titiler.openeo.factory import EndpointsFactory
 from titiler.openeo.middleware import DynamicCacheControlMiddleware
 from titiler.openeo.services import get_store, get_tile_store, get_udp_store
 from titiler.openeo.settings import ApiSettings, AuthSettings, BackendSettings
-from titiler.openeo.stacapi import stacApiBackend
 
 from .. import __version__ as titiler_version
 from .processes import PROCESS_SPECIFICATIONS, process_registry
 from .processes.implementations.io import LoadCollection
+from .stacapi import stacApiBackend
 
-STAC_VERSION = "1.0.0"
+STAC_VERSION = "1.1.0"
 
 api_settings = ApiSettings()
 auth_settings = AuthSettings()
