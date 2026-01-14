6f74dd13d3a28351fbd79defc10944ca76e7350a
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2026-01-14 10:47:15 +0100

    Revert "feat: implement S3 and composite S3+Redis cache backends"
    This reverts commit 1afa9423d0febab35115d131c7d1b1c4a81caafb.

--- titiler/cache/backends/__init__.py
+++ titiler/cache/backends/__init__.py
@@ -1,15 +1,5 @@
-"""Cache backend implementations."""
+"""Cache backend abstract interface."""
 
 from .base import CacheBackend, CacheBackendUnavailable, CacheError
-from .redis import RedisCacheBackend
-from .s3 import S3StorageBackend
-from .s3_redis import S3RedisCacheBackend
 
-__all__ = [
-    "CacheBackend",
-    "CacheError",
-    "CacheBackendUnavailable",
-    "RedisCacheBackend",
-    "S3StorageBackend",
-    "S3RedisCacheBackend",
-]
+__all__ = ["CacheBackend", "CacheError", "CacheBackendUnavailable"]
