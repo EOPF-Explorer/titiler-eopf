e6377dda80e88f34b26f0c8993ec11fa5c7c8ec6
vincentsarago <vincent.sarago@gmail.com>
2026-03-20 08:43:43 +0100

    fix tests

--- tests/test_openeo_processes.py
+++ tests/test_openeo_processes.py
@@ -1,9 +1,19 @@
 """titiler-eopf openeo test processes"""
 
+import json
+import os
+
+import pystac
+from openeo_pg_parser_networkx.pg_schema import BoundingBox
 from rio_tiler.models import ImageData
 
 from titiler.eopf.openeo.processes import PROCESS_IMPLEMENTATIONS, process_registry
 from titiler.eopf.openeo.processes.implementations import load_zarr
+from titiler.eopf.openeo.stacapi import LoadCollection
+
+FIXTURES_DIRECTORY = os.path.join(os.path.dirname(__file__), "fixtures")
+
+collection_json = os.path.join(FIXTURES_DIRECTORY, "collection.json")
 
 
 def test_custom_processes():
@@ -14,8 +24,8 @@ def test_custom_processes():
 
 def test_registery():
     """Check load_zarr is in the registery"""
-    _, last_registered = list(process_registry)[-1]
-    assert last_registered == "load_zarr"
+    _, names = zip(*process_registry)
+    assert "load_zarr" in names
     assert process_registry.get("load_zarr")
 
 
@@ -64,3 +74,54 @@ def test_load_zarr(geozarr_dataset):
     assert len(img_stack) == 1
     assert isinstance(img_stack[0], ImageData)
     assert img_stack[0].band_descriptions == ["/measurements/reflectance:b02"]
+
+
+def test_load_collection(geozarr_stac):
+    """Test load collection process."""
+
+    class FakeBackend:
+        url: str
+
+        def __init__(self, url: str):
+            self.url = url
+
+        def get_items(self, *args, **kwargs):
+            return [pystac.Item.from_dict(json.loads(geozarr_stac))]
+
+    loaders = LoadCollection(FakeBackend("https://fake.api.io/stac"))
+
+    bbox = BoundingBox(
+        west=14.941406249999993,
+        south=37.857507156252034,
+        east=15.11718749999999,
+        north=37.99616267972812,
+    )
+
+    collection = loaders.load_collection(
+        "sentinel-2-l2a",
+        spatial_extent=bbox,
+        width=512,
+        height=512,
+        bands=[
+            "reflectance|bands=b04",
+            "reflectance|bands=b03",
+            "reflectance|bands=b02",
+        ],
+    )
+    assert collection.band_names == [
+        "reflectance|bands=b04",
+        "reflectance|bands=b03",
+        "reflectance|bands=b02",
+    ]
+    assert collection.width == 512
+    assert collection.height == 512
+
+    # Check we can get values (ImageData objects)
+    img_stack = list(collection.values())
+    assert len(img_stack) == 1
+    assert isinstance(img_stack[0], ImageData)
+    assert img_stack[0].band_descriptions == [
+        "reflectance_b04",
+        "reflectance_b03",
+        "reflectance_b02",
+    ]
